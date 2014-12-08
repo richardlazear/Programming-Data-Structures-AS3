46712bb322114ff9ecf4988ca298a2bd92fe4c4f
Richard Lazear <richard.g.lazear@gmail.com>
2014-12-08 02:25:23 -0600

    added description to Word.java

--- src/Word.java
+++ src/Word.java
@@ -1,3 +1,10 @@
+/************************************************************************************************************************************************************
+ This class is an object that stores only 2 fields: a string for a word from a file, and an integer for the number of times
+ that word appears in the file.  This class is used in the program so that both of these fields can be stored in a single
+ array slot in the main class.
+ ************************************************************************************************************************************************************/
+
+
 public class Word {
 	// Fields
 	public String word;
